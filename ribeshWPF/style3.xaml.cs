c0f492469df501cec2e650666f6f1e9d20e5d099
edangol <edangol@EVAN1>
2018-08-26 19:18:49 +0545

    practice

---
+++ ribeshWPF/style3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ribeshWPF
+{
+    /// <summary>
+    /// Interaction logic for style3.xaml
+    /// </summary>
+    public partial class style3 : Window
+    {
+        public style3()
+        {
+            InitializeComponent();
+        }
+    }
+}
